05de3f5eabdc13f9fd662f6258bc4210189e084a
MalFaraj1 <162789207+MalFaraj1@users.noreply.github.com>
2024-09-24 10:01:40 +0300

    2nd part completed - ui design

---
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Conversation() {
+  return (
+    <>
+        <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
+            <div className='avatar online'>
+                <div className='w-12 rounded-full'>
+                    <img />
+                </div>
+            </div>
+            <div className='flex flex-col flex-1'>
+                <div className='flex gap-3 justify-between'>
+                    <p className='font-bold text-gary-200'>Mhammad</p>
+                    <span className='text-xl'>!!!</span>
+                </div>
+            </div>
+        </div>
+
+        <div className='divider my-0 py-6 h-1'>
+
+        </div>
+    </>
+  )
+}
+
+export default Conversation;
